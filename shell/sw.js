333153564bb845d8ebb2beabbea2c32c937bd3b6
Prashanth A <prashanth.a@PW-C02Q11GCG8WN.local>
2018-02-27 19:11:17 +0530

    update build

--- shell/sw.js
+++ shell/sw.js
@@ -1,14 +1,14 @@
-const PRECACHE = 'precache-v17';
+const PRECACHE = 'precache-v18';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
 	'index.html',
 	'./',
-	'./#',
-	'bundle.c902a.js',
-	'style.d0d1d.css',
+	'bundle.6c060.js',
+	'style.2835c.css',
 	'assets/close.png',
+	'assets/arrow.png',
 	'assets/comment.png',
 	'assets/icon.png',
 	'assets/plus.png',
